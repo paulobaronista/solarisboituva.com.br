f678560cf6d392aa9ac28f72976d10b09ba0b7e9
PauloBaronista <paulo@rpm.com.br>
2021-07-22 19:16:00 -0300

    clone

---
+++ application/views/contato_insucesso.php
@@ -0,0 +1,9 @@
+<div id="aviso" class="aviso text-center">
+    <div>
+        <button type="button" class="close" id="close"><span aria-hidden="true">&times;</span></button>
+        <p>O e-mail não pode ser enviado!<br />
+            <strong>Tente novamente!</strong><br />
+            Se o problema persistir, entre em contato pelo email <strong>contato@bcanton.com.br</strong>
+        </p>
+    </div>
+</div>
