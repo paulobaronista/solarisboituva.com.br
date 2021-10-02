f322329244adfb53ec6b6ede9d1d3282bf82149e
PauloBaronista <paulo@rpm.com.br>
2021-10-02 15:21:07 -0300

    Google Analytics

--- application/views/html_footer.php
+++ application/views/html_footer.php
@@ -14,6 +14,20 @@ echo script_tag('assets/js/jquery.inview.js');
     <a href="https://api.whatsapp.com/send?1=pt_BR&phone=5515996810304" target="_blank" class="whats">Whatsapp</a>
 </div>
 
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-M6N61CLJ8R"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-M6N61CLJ8R');
+</script>
+
 </body>
 
 </html>
